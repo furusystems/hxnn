9f8e4ea909832a14dc90487c585827ff3a15139e
Michel Käser <mk@frontender.ch>
2014-06-11 17:47:37 +0200

    correct way to include external header files

--- src/NanoMsg.cpp
+++ src/NanoMsg.cpp
@@ -1,17 +1,17 @@
 #include "NanoMsg.hpp"
 
-#include "nanomsg/nn.h"
+#include <nanomsg/nn.h>
 // scalability protocols
-#include "nanomsg/pair.h"
-#include "nanomsg/reqrep.h"
-#include "nanomsg/pubsub.h"
-#include "nanomsg/survey.h"
-#include "nanomsg/pipeline.h"
-#include "nanomsg/bus.h"
+#include <nanomsg/pair.h>
+#include <nanomsg/reqrep.h>
+#include <nanomsg/pubsub.h>
+#include <nanomsg/survey.h>
+#include <nanomsg/pipeline.h>
+#include <nanomsg/bus.h>
 // transport mechanisms
-#include "nanomsg/inproc.h"
-#include "nanomsg/ipc.h"
-#include "nanomsg/tcp.h"
+#include <nanomsg/inproc.h>
+#include <nanomsg/ipc.h>
+#include <nanomsg/tcp.h>
 
 extern "C"
 {
