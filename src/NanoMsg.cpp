696d9e29321e8d4b0c887bcf98c462afc176d844
Michel Käser <mk@frontender.ch>
2014-06-11 14:01:57 +0200

    merge stashed changes

--- src/NanoMsg.cpp
+++ src/NanoMsg.cpp
@@ -39,7 +39,7 @@ extern "C"
         int ret = nn_bind(val_socket(sock), val_string(address));
         if (ret < 0) {
             val_throw(alloc_int(nn_errno()));
-            val = alloc_int(0);
+            val = alloc_int(999);
         } else {
             val = alloc_int(ret);
         }
@@ -53,7 +53,7 @@ extern "C"
         val_check(sock, socket);
         finalize_socket(sock);
 
-       return alloc_int(0);
+       return alloc_int(999);
     }
 
     // http://nanomsg.org/v0.3/nn_connect.3.html
@@ -66,7 +66,7 @@ extern "C"
         int ret = nn_connect(val_socket(sock), val_string(address));
         if (ret < 0) {
             val_throw(alloc_int(nn_errno()));
-            val = alloc_int(0);
+            val = alloc_int(999);
         } else {
             val = alloc_int(ret);
         }
@@ -87,6 +87,36 @@ extern "C"
         int ret = nn_getsockopt(val_socket(sock), val_int(level), val_int(option), &val, &valsize);
         if (ret < 0) {
             val_throw(alloc_int(nn_errno()));
+            val = alloc_int(999);
+        }
+
+        return val;
+    }
+
+    // http://nanomsg.org/v0.4/nn_poll.3.html
+    static value hxnn_poll(value socks, value timeout)
+    {
+        val_check(socks, array);
+        val_check(timeout, float);
+
+        const size_t arr_size = val_array_size(socks);
+        struct nn_pollfd pfd[arr_size];
+
+        value current;
+        for (int i = 0; i < arr_size; ++i)
+        {
+            current = val_array_i(socks, i);
+            val_check(current, socket);
+            pfd[i].fd     = val_socket(current);
+            pfd[i].events = NN_POLLIN | NN_POLLOUT;
+        }
+
+        value val;
+        int ret = nn_poll(pfd, arr_size, val_float(timeout));
+        if (ret == -1) {
+            val_throw(alloc_int(nn_errno()));
+            val = alloc_int(999);
+        } else if (ret == 0) {
             val = alloc_int(0);
         }
 
@@ -105,7 +135,7 @@ extern "C"
         int recv = nn_recv(val_socket(sock), &buf, sizeof(buf), val_int(flags));
         if (recv < 0) {
             val_throw(alloc_int(nn_errno()));
-            val = alloc_int(0);
+            val = alloc_int(999);
         } else {
             val = alloc_string(buf);
         }
@@ -143,7 +173,7 @@ extern "C"
         int sent = nn_send(val_socket(sock), val_string(message), val_strlen(message) + 1, val_int(flags)); // +1 for \0
         if (sent < 0) {
             val_throw(alloc_int(nn_errno()));
-            val = alloc_int(0);
+            val = alloc_int(999);
         } else {
             val = alloc_int(sent);
         }
@@ -173,7 +203,7 @@ extern "C"
             val_throw(alloc_int(nn_errno()));
         }
 
-        return alloc_int(0);
+        return alloc_int(999);
     }
 
     // http://nanomsg.org/v0.3/nn_shutdown.3.html
@@ -187,7 +217,7 @@ extern "C"
             val_throw(alloc_int(nn_errno()));
         }
 
-        return alloc_int(0);
+        return alloc_int(999);
     }
 
     // http://nanomsg.org/v0.3/nn_socket.3.html
@@ -200,7 +230,7 @@ extern "C"
         int ret = nn_socket(val_int(domain), val_int(protocol));
         if (ret < 0) {
             val_throw(alloc_int(nn_errno()));
-            val = alloc_int(0);
+            val = alloc_int(999);
         } else {
             val = alloc_socket(ret);
             val_gc((value)&val, finalize_socket);
